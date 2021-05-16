839cb4339b118ea56b00f335f95949794f15a45b
akrma Assi <akram.20002010@gmail.com>
2021-05-16 23:11:54 +0300

    add  Query for args url

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask,request,jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/api',methods=['GET'])
 def hello():
-    return "Hello dsd!"
+    d={}
+    d["Query"]=str(request.args["Query"])
+    return d["Query"]
+    
